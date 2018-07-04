d9bf520877c55217ad19617e0765771a3ffa5b74
Alex <info@alex-olear.de>
2018-07-04 21:53:24 +0200

    initial

---
+++ main.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include "subnet.h"
+#include <iomanip>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+
+template<typename T> void printElement(T t, const int& width) {
+  std::cout << "| "<< std::left << std::setw(width) << std::setfill(' ') << t;
+}
+
+void printNet(const std::vector<subnet::ValueNetwork>& vec) {
+  std::cout<<"+";
+  std::cout<< std::left << std::setw(84) << std::setfill('-') << "";
+  std::cout<<"+"<<std::endl;
+  printElement("NetID", 15);
+  printElement("FirstAddr", 15);
+  printElement("Broadcast", 15);
+  printElement("NetMask", 15);
+  printElement("MaxHosts", 15);
+  std::cout<<"|"<<std::endl;
+  std::cout<<"+";
+  std::cout<< std::left << std::setw(84) << std::setfill('-') << "";
+  std::cout<<"+"<<std::endl;
+
+  for(const auto& rn : vec) {
+    printElement(subnet::toIP(rn.NetId), 15);
+    printElement(subnet::toIP(rn.FirstAddr), 15);
+    printElement(subnet::toIP(rn.BroadcastAddr), 15);
+    printElement(subnet::toIP(rn.Mask), 15);
+    printElement(rn.MaxHosts, 15);
+    std::cout<<"|"<<std::endl;
+  }
+
+  std::cout<<"+";
+  std::cout<< std::left << std::setw(84) << std::setfill('-') << "";
+  std::cout<<"+"<<std::endl;
+
+}
+
+int main(int argc, char **argv) {
+  long int netcount = 1;
+  bool help = false;
+  std::string ipstr;
+
+  for(int i = 1; i < argc; ++i) {
+    if(std::strcmp("-n", argv[i]) == 0)
+      netcount = atol(argv[i+1]);
+
+    if(std::strcmp("-h", argv[i]) == 0) {
+      help = true;
+      break;
+    }
+
+    if(std::strcmp("-ip", argv[i]) == 0)
+      ipstr = argv[i+1];
+
+    if(std::strcmp("-m", argv[i]) == 0);
+  }
+
+  if(help) {
+    std::cout<<"subcalc[OPTION][ARG]..."<<std::endl;
+    std::cout<<"OPTIONS:"<<std::endl;
+    std::cout<<"\t -n\t[subnet count]"<<std::endl;
+    std::cout<<"\t -ip\t[ip address]"<<std::endl;
+    std::cout<<"\t -h\tshow help"<<std::endl;
+    std::cout<<"\t -m\t[mask <i>]"<<std::endl;
+    return 0;
+  }
+
+  std::vector<size_t> maxhost_in_sub;
+  for(int i = 0; i < netcount; ++i) {
+    std::string input;
+    std::cout<<"Subnet "<<i+1<<" max. hosts: ";
+    std::getline(std::cin, input);
+    maxhost_in_sub.push_back(std::stol(input));
+  }
+
+
+  std::vector<subnet::ValueNetwork> vec;
+  std::sort(maxhost_in_sub.rbegin(), maxhost_in_sub.rend());
+  bool first = true;
+  subnet::ValueNetwork r;
+  for(auto e : maxhost_in_sub) {
+    if(first) {
+      r = subnet::calculateVal(subnet::toValue(ipstr), e);
+      first = false;
+    }
+    else
+      r = subnet::calculateVal(r.BroadcastAddr + 1, e);
+
+    vec.push_back(r);
+  }
+
+  printNet(vec);
+
+
+  return 0;
+}
---
+++ subnet.h
@@ -0,0 +1,67 @@
+#ifndef SUBNET_H
+#define SUBNET_H
+
+#include <math.h>
+#include <bitset>
+#include <sstream>
+#include <string>
+
+namespace subnet {
+  struct ValueNetwork {
+    std::uint32_t NetId;
+    std::uint32_t FirstAddr;
+    std::uint32_t BroadcastAddr;
+    std::uint32_t Mask;
+    size_t MaxHosts;
+  };
+
+  std::uint32_t calcMask (size_t hosts) {
+    std::uint32_t a = std::pow(2, std::ceil(std::log2(hosts + 2)));
+    std::uint32_t b = UINT32_MAX;
+
+    return ~(b & (a - 1));
+  }
+
+  ValueNetwork calculateVal(const std::uint32_t& ip, size_t hosts) {
+    ValueNetwork net;
+    auto mask = calcMask(hosts);
+    auto addr = ip & mask;
+
+    net.NetId = addr;
+    net.FirstAddr = addr + 1;
+    net.BroadcastAddr = ((ip & mask) +
+                         static_cast<uint32_t>(std::pow(2, std::ceil(std::log2(hosts + 2))))) - 1;
+    net.Mask = mask;
+    net.MaxHosts = std::pow(2, std::ceil(std::log2(hosts))) - 2;
+
+    return net;
+  }
+
+  std::string toIP(std::uint32_t addr) {
+    std::string str;
+    str += std::to_string(addr >> 24) + ".";
+    str += std::to_string(((addr << 8) >> 8) >> 16) + ".";
+    str += std::to_string(((addr << 16) >> 16) >> 8) + ".";
+    str += std::to_string((addr << 24) >> 24);
+    return str;
+  }
+
+  std::uint32_t toValue(const std::string& ip) {
+    std::uint32_t ret = 0;
+    std::string ipstr = ip;
+
+    std::string token;
+    std::istringstream iss(ipstr);
+    auto pot = 24;
+    while(getline(iss, token, '.')) {
+      std::uint32_t val = std::stoi(token);
+      val = val << pot;
+      ret |= val;
+      pot -= 8;
+    }
+
+    return ret;
+  }
+};
+
+#endif
